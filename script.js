10f2076f841b607bc967a8b58d2992066afd056d
socry <leonardosbo02@gmail.com>
2023-03-13 13:15:09 -0300

    Add function to fetch api info

---
+++ script.js
@@ -0,0 +1,33 @@
+const apiKey = "add84e844eabefa7b3ae5594556d93e8";
+
+async function getWeather(location) {
+  let data = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`,
+    { mode: "cors" }
+  );
+  return await data.json();
+}
+let searchInput = document.querySelector("#searchInput");
+let nameDisplayed = document.querySelector("#locationName");
+let weatherIcon = document.querySelector("#weatherIcon");
+let temperature = document.querySelector("#temperature>#text");
+let infoDisplay = document.querySelector("#info");
+
+const searchButton = document.querySelector("#searchButton");
+
+searchButton.addEventListener("click", () => {
+  let locationName = document.querySelector("#searchInput").value;
+  getWeather(locationName).then((info) => {
+    try {
+      let iconCode = info["weather"][0]["icon"];
+
+      nameDisplayed.textContent = info["name"];
+      weatherIcon.src = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+      temperature.textContent = info["main"]["temp"];
+      infoDisplay.style.display = "flex";
+    } catch (err) {
+      console.log(err);
+      searchInput.placeholder = "Invalid location";
+    }
+  });
+});
